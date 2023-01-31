5872893bf21611359b781d4639e815495edc645e
Sylvester254 <slynganga59@gmail.com>
2023-01-31 14:14:36 +0300

    update

--- 0x01-ES6_promise/3-all.js
+++ 0x01-ES6_promise/3-all.js
@@ -1,11 +1,11 @@
-import {uploadPhoto,createUser} from './utils.js';
+import { uploadPhoto, createUser } from './utils.js';
 
 export default function handleProfileSignup() {
-    return Promise
-        .all([uploadPhoto(),createUser()])
-        .then(([photoResponse,userResponse]) => {
-            console.log(`${photoResponse.body} ${userResponse.firstName} ${userResponse.lastName}`);
-        }).catch(() =>{
-            console.log('Signup system offline');
-        })
+  return Promise
+    .all([uploadPhoto(), createUser()])
+    .then(([photoResponse, userResponse]) => {
+      console.log(`${photoResponse.body} ${userResponse.firstName} ${userResponse.lastName}`);
+    }).catch(() => {
+      console.log('Signup system offline');
+    });
 }
